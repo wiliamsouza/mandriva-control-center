7f6411bd75a2efbce4ed44ce3c7cf2e656f0f231
wiliam <wiliam@aeaf0dc8-91d7-0310-8701-e1fc1a66ab8d>
2011-01-26 20:35:47 +0000

    Changed interfaces methods, added more users examples
    git-svn-id: https://camelo.conectiva/repos/projetos/qtlab/mcc2@8616 aeaf0dc8-91d7-0310-8701-e1fc1a66ab8d

---
+++ mcc2/backends/users/examples/add-group.py
@@ -0,0 +1,8 @@
+import dbus
+bus = dbus.SystemBus()
+proxy = bus.get_object(
+    'org.mandrivalinux.mcc2.Users',
+    '/org/mandrivalinux/mcc2/Users')
+interface = dbus.Interface(proxy, 'org.mandrivalinux.mcc2.Users')
+result = interface.AddGroup('test')
+print result['name'], result['gid']
--- mcc2/backends/users/service.py
+++ mcc2/backends/users/service.py
@@ -29,18 +29,15 @@ class Users(dbus.service.Object):
                          connection_keyword='connection')
     def AddGroup(self, group, sender, connection):
         self.check_authorization(sender, connection,
-            'org.mandrivalinux.mcc2.users.AddGroup')
+            'org.mandrivalinux.mcc2.users.addgroup')
         init_group = self.__libuser.initGroup(group)
-        result = 0
         try:
-            result = self.__libuser.addGroup(init_group)
+            self.__libuser.addGroup(init_group)
         except RuntimeError, error:
-            if error == 'entry already present in file':
-                msg = 'org.mandrivalinux.mcc2.Users.Error.GroupAlreadyExist'
-                raise dbus.DBusException, msg
-        # FIXME: What's the others return code for addGroup?
-        if result != 1:
             msg = 'org.mandrivalinux.mcc2.Users.Error.AddGroupFailed'
+            #FIXME: What's the others libuser errors?
+            if str(error) == 'entry already present in file':
+                msg = 'org.mandrivalinux.mcc2.Users.Error.GroupAlreadyExist'
             raise dbus.DBusException, msg
         group_entity = self.__libuser.lookupGroupByName(group)
         gid = group_entity.get(libuser.GIDNUMBER)[0]
@@ -48,32 +45,98 @@ class Users(dbus.service.Object):
         return {'gid': gid, 'name': name}
 
     @dbus.service.method("org.mandrivalinux.mcc2.Users",
+                         in_signature='s',
+                         out_signature='a{sv}',
+                         sender_keyword='sender',
+                         connection_keyword='connection')
+    def AddUser(self, user, sender, connection):
+        self.check_authorization(sender, connection,
+            'org.mandrivalinux.mcc2.users.adduser')
+        init_user = self.__libuser.initUser(user)
+        try:
+            self.__libuser.addUser(init_user)
+        except RuntimeError, error:
+            msg = 'org.mandrivalinux.mcc2.Users.Error.AddUserFailed'
+            # FIXME: What's the others libuser errors?
+            if str(error) == 'entry already present in file':
+                msg = 'org.mandrivalinux.mcc2.Users.Error.UserAlreadyExist'
+            raise dbus.DBusException, msg
+        user_entity = self.__libuser.lookupUserByName(user)
+        uid = user_entity.get(libuser.UIDNUMBER)[0]
+        name = user_entity.get(libuser.USERNAME)[0]
+        return {'uid': uid, 'name': name}
+
+    @dbus.service.method("org.mandrivalinux.mcc2.Users",
+                         in_signature='s',
+                         out_signature='a{sv}',
+                         sender_keyword='sender',
+                         connection_keyword='connection')
+    def DeleteGroup(self, group, sender, connection):
+        self.check_authorization(sender, connection,
+            'org.mandrivalinux.mcc2.users.deletegroup')
+        group_entity = self.__libuser.lookupGroupByName(group)
+        gid = group_entity.get(libuser.GIDNUMBER)[0]
+        name = group_entity.get(libuser.GROUPNAME)[0]
+        init_group = self.__libuser.initGroup(group)
+        try:
+            self.__libuser.deleteGroup(init_group)
+        except RuntimeError, error:
+            msg = 'org.mandrivalinux.mcc2.Users.Error.DeleteGroupFailed'
+            raise dbus.DBusException, msg
+        return {'gid': gid, 'name': name}
+
+    @dbus.service.method("org.mandrivalinux.mcc2.Users",
+                         in_signature='s',
+                         out_signature='a{sv}',
+                         sender_keyword='sender',
+                         connection_keyword='connection')
+    def DeleteUser(self, user, sender, connection):
+        self.check_authorization(sender, connection,
+            'org.mandrivalinux.mcc2.users.deleteuser')
+        user_entity = self.__libuser.lookupUserByName(user)
+        uid = user_entity.get(libuser.UIDNUMBER)[0]
+        name = user_entity.get(libuser.USERNAME)[0]
+        init_user = self.__libuser.initUser(user)
+        try:
+            self.__libuser.deleteUser(init_user)
+        except RuntimeError, error:
+            msg = 'org.mandrivalinux.mcc2.Users.Error.DeleteUserFailed'
+            raise dbus.DBusException, msg
+        return {'uid': uid, 'name': name}
+
+    #TODO: Rename to ListGroups
+    @dbus.service.method("org.mandrivalinux.mcc2.Users",
                          out_signature='as')
     def Groups(self):
         return self.__libuser.enumerateGroups()
 
+    #TODO: Rename to ListGroupsByUser
     @dbus.service.method("org.mandrivalinux.mcc2.Users",
                          in_signature='s',
                          out_signature='as')
     def GroupsByUser(self, user):
         return self.__libuser.enumerateGroupsByUser(user)
 
+    #FIXME: Remove this method should be used only internaly
     @dbus.service.method("org.mandrivalinux.mcc2.Users",
                          out_signature='as')
     def GroupsFull(self):
         return self.__libuser.enumerateGroupsByUser()
 
+    #TODO: Rename to ListUsers
     @dbus.service.method("org.mandrivalinux.mcc2.Users",
                          out_signature='as')
     def Users(self):
         return self.__libuser.enumerateUsers()
 
+    #TODO: Rename to ListUsersByGroup
     @dbus.service.method("org.mandrivalinux.mcc2.Users",
                          in_signature='s',
                          out_signature='as')
     def UsersByGroup(self, user):
         return self.__libuser.enumerateUsersByGroup(user)
 
+    #FIXME: Remove this method should be used only internaly
     @dbus.service.method("org.mandrivalinux.mcc2.Users",
                          out_signature='as')
     def UsersFull(self):
