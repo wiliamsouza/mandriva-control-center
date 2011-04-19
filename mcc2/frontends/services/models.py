68a3df743ad5abcc9b3150a2a419815605211abe
wiliam <wiliam@aeaf0dc8-91d7-0310-8701-e1fc1a66ab8d>
2011-04-19 18:04:16 +0000

    Removed PySide code, Added sort services support
    git-svn-id: https://camelo.conectiva/repos/projetos/qtlab/mcc2@9008 aeaf0dc8-91d7-0310-8701-e1fc1a66ab8d

--- mcc2/frontends/services/models.py
+++ mcc2/frontends/services/models.py
@@ -5,7 +5,7 @@ import dbus
 import dbus.mainloop.glib
 dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 
-bus = dbus.SystemBus()
+bus = dbus.SystemBus()#private=True)
 proxy = bus.get_object('org.mandrivalinux.mcc2.Services',
                        '/org/mandrivalinux/mcc2/Services')
 interface = dbus.Interface(proxy, 'org.mandrivalinux.mcc2.Services')
@@ -21,9 +21,9 @@ class Service(QtCore.QObject):
         self.__servicePath = servicePath
         self.__serviceDetails = serviceDetails
 
-        properties_proxy = bus.get_object('org.freedesktop.systemd1', self.__servicePath)
-        properties_interface = dbus.Interface(properties_proxy, 'org.freedesktop.DBus.Properties')
-        properties_interface.connect_to_signal('PropertiesChanged', self.on_properties_changed)
+        self.properties_proxy = bus.get_object('org.freedesktop.systemd1', self.__servicePath)
+        self.properties_interface = dbus.Interface(self.properties_proxy, 'org.freedesktop.DBus.Properties')
+        self.properties_interface.connect_to_signal('PropertiesChanged', self.on_properties_changed)
 
     def __getName(self):
         return self.__serviceDetails['Id']
@@ -48,7 +48,6 @@ class Service(QtCore.QObject):
                 print "timed out"
             if error.get_dbus_name() == "org.mandrivalinux.mcc2.Services.Error.NotAuthorized":
                 print 'Not Authorized'
-        self.update()
 
     def stop(self):
         try:
@@ -58,17 +57,15 @@ class Service(QtCore.QObject):
                 print "Timed out"
             if error.get_dbus_name() == "org.mandrivalinux.mcc2.Services.Error.NotAuthorized":
                 print 'Not Authorized'
-        self.update()
 
     def restart(self):
         try:
             interface.Restart(self.name, 'fail')
         except dbus.exceptions.DBusException, error:
             if error.get_dbus_name() == "org.freedesktop.DBus.Error.NoReply":
-                print "timed out"
+               print "timed out"
             if error.get_dbus_name() == "org.mandrivalinux.mcc2.Services.Error.NotAuthorized":
                 print 'Not Authorized'
-        self.update()
 
     def on_properties_changed(self, *args, **kargs):
         self.update()
@@ -79,14 +76,6 @@ class Service(QtCore.QObject):
 
     #changed = QtCore.Signal()
     changed = QtCore.pyqtSignal()
-
-    """
-    name = QtCore.Property(unicode, __getName, notify=changed)
-    description = QtCore.Property(unicode, __getDescription, notify=changed)
-    loadState = QtCore.Property(unicode, __getLoadState, notify=changed)
-    activeState = QtCore.Property(unicode, __getActiveState, notify=changed)
-    subState = QtCore.Property(unicode, __getSubState, notify=changed)
-    """
 
     name = QtCore.pyqtProperty(unicode, __getName, notify=changed)
     description = QtCore.pyqtProperty(unicode, __getDescription, notify=changed)
@@ -124,13 +113,11 @@ class ServiceModel(QtCore.QAbstractListModel):
         service.restart()
 
     def populate(self):
-        count = 0
+        services = []
         for servicePath in interface.List():
             serviceDetails = interface.ServiceDetails(servicePath[6])
             # This filtering is done to avoid show up more than one .device unit
             # under different names
             if serviceDetails['Following'] == "":
-                self.__services.append(Service(servicePath[6], serviceDetails, parent=self.parent))
-            else:
-                count = count + 1
-        #print '%d .device unit removed by duplicity' % count
+                services.append(Service(servicePath[6], serviceDetails, parent=self.parent))
+        self.__services = sorted(services, key=lambda service: service.name)
