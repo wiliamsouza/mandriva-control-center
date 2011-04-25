3caf944f55c7130567a755b9da173d4fdaea74d1
wiliam <wiliam@aeaf0dc8-91d7-0310-8701-e1fc1a66ab8d>
2011-04-25 14:02:53 +0000

    Added i18n folder
    git-svn-id: https://camelo.conectiva/repos/projetos/qtlab/mcc2@9026 aeaf0dc8-91d7-0310-8701-e1fc1a66ab8d

---
+++ mcc2/frontends/services/views/i18n/SystemServices_de.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>SystemServices</name>
+    <message>
+        <location filename="../SystemServices.qml" line="136"/>
+        <source>Status:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SystemServices.qml" line="169"/>
+        <source>Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SystemServices.qml" line="178"/>
+        <source>Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SystemServices.qml" line="187"/>
+        <source>Restart</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ mcc2/frontends/services/views/i18n/SystemServices_pt_BR.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_BR" sourcelanguage="en_US">
+<context>
+    <name>SystemServices</name>
+    <message>
+        <location filename="../SystemServices.qml" line="136"/>
+        <source>Status:</source>
+        <translation>Estado:</translation>
+    </message>
+    <message>
+        <location filename="../SystemServices.qml" line="169"/>
+        <source>Start</source>
+        <translation>Iniciar</translation>
+    </message>
+    <message>
+        <location filename="../SystemServices.qml" line="178"/>
+        <source>Stop</source>
+        <translation>Parar</translation>
+    </message>
+    <message>
+        <location filename="../SystemServices.qml" line="187"/>
+        <source>Restart</source>
+        <translation>Reiniciar</translation>
+    </message>
+</context>
+</TS>
