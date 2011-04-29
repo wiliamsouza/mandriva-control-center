7586e9125d94bfff138ffcb80679ff73fb59656f
Wiliam Souza <wiliam@mandriva.com>
2011-04-29 00:55:15 -0300

    Removed PySide slots declarations

--- mcc2/frontends/services/controllers.py
+++ mcc2/frontends/services/controllers.py
@@ -1,27 +1,26 @@
-#from PySide import QtCore
 from PyQt4 import QtCore
+
 
 class Controller(QtCore.QObject):
 
     def __init__(self, parent=None):
         QtCore.QObject.__init__(self, parent)
 
-    #@QtCore.Slot(QtCore.QObject, int)
     @QtCore.pyqtSlot(QtCore.QObject, int)
     def start_service(self, serviceModel, currentIndex):
         serviceModel.start(currentIndex)
 
-    #@QtCore.Slot(QtCore.QObject, int)
     @QtCore.pyqtSlot(QtCore.QObject, int)
     def stop_service(self, serviceModel, currentIndex):
         serviceModel.stop(currentIndex)
 
-    #@QtCore.Slot(QtCore.QObject, int)
     @QtCore.pyqtSlot(QtCore.QObject, int)
     def restart_service(self, serviceModel, currentIndex):
         serviceModel.restart(currentIndex)
 
-    #@QtCore.Slot(QtCore.QObject, str)
     @QtCore.pyqtSlot(QtCore.QObject, str)
     def search(self, proxyServiceModel, text):
-        proxyServiceModel.setFilterRegExp(QtCore.QRegExp(text, QtCore.Qt.CaseInsensitive, QtCore.QRegExp.FixedString))
+        regex = QtCore.QRegExp(text,
+                               QtCore.Qt.CaseInsensitive,
+                               QtCore.QRegExp.FixedString)
+        proxyServiceModel.setFilterRegExp(regex)
