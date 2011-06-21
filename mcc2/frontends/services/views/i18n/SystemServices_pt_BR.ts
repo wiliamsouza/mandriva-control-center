77f022e6d59eef04472c5cf17f4076b1159871a2
Wiliam Souza <wiliam@mandriva.com>
2011-06-21 12:06:11 -0300

    Updated pt_BR translations

--- mcc2/frontends/services/views/i18n/SystemServices_pt_BR.ts
+++ mcc2/frontends/services/views/i18n/SystemServices_pt_BR.ts
@@ -1,42 +1,57 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="pt_BR" sourcelanguage="en_US">
+<TS version="2.0" language="pt_BR">
 <context>
     <name>SystemServices</name>
     <message>
-        <location filename="../SystemServices.qml" line="32"/>
+        <location filename="../SystemServices.qml" line="30"/>
         <source>All</source>
         <translation>Tudo</translation>
     </message>
     <message>
-        <location filename="../SystemServices.qml" line="40"/>
+        <location filename="../SystemServices.qml" line="38"/>
         <source>Active</source>
         <translation>Ativo</translation>
     </message>
     <message>
-        <location filename="../SystemServices.qml" line="48"/>
+        <location filename="../SystemServices.qml" line="46"/>
         <source>Inactive</source>
         <translation>Inativo</translation>
     </message>
     <message>
-        <location filename="../SystemServices.qml" line="56"/>
+        <location filename="../SystemServices.qml" line="54"/>
         <source>Failed</source>
         <translation>Com falha</translation>
     </message>
     <message>
-        <location filename="../SystemServices.qml" line="143"/>
+        <location filename="../SystemServices.qml" line="144"/>
         <source>Start</source>
         <translation>Iniciar</translation>
     </message>
     <message>
-        <location filename="../SystemServices.qml" line="150"/>
+        <location filename="../SystemServices.qml" line="151"/>
         <source>Stop</source>
         <translation>Parar</translation>
     </message>
     <message>
-        <location filename="../SystemServices.qml" line="157"/>
+        <location filename="../SystemServices.qml" line="158"/>
         <source>Restart</source>
         <translation>Reiniciar</translation>
+    </message>
+    <message>
+        <location filename="../SystemServices.qml" line="204"/>
+        <source>Status</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../SystemServices.qml" line="212"/>
+        <source>Service</source>
+        <translation>Serviço</translation>
+    </message>
+    <message>
+        <location filename="../SystemServices.qml" line="220"/>
+        <source>Description</source>
+        <translation>Descrição</translation>
     </message>
 </context>
 </TS>
--- mcc2/frontends/users/views/i18n/UsersAndGroups_pt_BR.ts
+++ mcc2/frontends/users/views/i18n/UsersAndGroups_pt_BR.ts
@@ -9,163 +9,174 @@
         <translation>Usuários</translation>
     </message>
     <message>
-        <location filename="../UsersAndGroups.qml" line="43"/>
+        <location filename="../UsersAndGroups.qml" line="47"/>
         <source>Groups</source>
         <translation>Grupos</translation>
     </message>
     <message>
-        <location filename="../UsersAndGroups.qml" line="55"/>
+        <location filename="../UsersAndGroups.qml" line="63"/>
         <source>Add user</source>
         <translation>Adicionar usuário</translation>
     </message>
     <message>
-        <location filename="../UsersAndGroups.qml" line="66"/>
+        <location filename="../UsersAndGroups.qml" line="75"/>
         <source>Add group</source>
         <translation>Adicionar grupo</translation>
     </message>
     <message>
-        <location filename="../UsersAndGroups.qml" line="194"/>
-        <source>User last changed password on</source>
-        <translation>Usuário modificou sua senha pela última vez em</translation>
-    </message>
-    <message>
-        <location filename="../UsersAndGroups.qml" line="207"/>
-        <location filename="../UsersAndGroups.qml" line="519"/>
+        <location filename="../UsersAndGroups.qml" line="189"/>
+        <location filename="../UsersAndGroups.qml" line="420"/>
         <source>Full name</source>
         <translation>Nome Completo</translation>
     </message>
     <message>
-        <location filename="../UsersAndGroups.qml" line="221"/>
-        <location filename="../UsersAndGroups.qml" line="533"/>
+        <location filename="../UsersAndGroups.qml" line="203"/>
+        <location filename="../UsersAndGroups.qml" line="434"/>
         <source>User name</source>
         <translation>Nome de usuário</translation>
     </message>
     <message>
-        <location filename="../UsersAndGroups.qml" line="235"/>
-        <location filename="../UsersAndGroups.qml" line="548"/>
+        <location filename="../UsersAndGroups.qml" line="218"/>
+        <location filename="../UsersAndGroups.qml" line="448"/>
         <source>Password</source>
         <translation>Senha</translation>
     </message>
     <message>
-        <location filename="../UsersAndGroups.qml" line="250"/>
-        <location filename="../UsersAndGroups.qml" line="563"/>
+        <location filename="../UsersAndGroups.qml" line="233"/>
+        <location filename="../UsersAndGroups.qml" line="463"/>
         <source>Confirm password</source>
         <translation>Confirmar senha</translation>
     </message>
     <message>
-        <location filename="../UsersAndGroups.qml" line="265"/>
-        <location filename="../UsersAndGroups.qml" line="578"/>
+        <location filename="../UsersAndGroups.qml" line="248"/>
+        <location filename="../UsersAndGroups.qml" line="478"/>
         <source>Login shell</source>
         <translation></translation>
     </message>
     <message>
         <location filename="../UsersAndGroups.qml" line="279"/>
-        <location filename="../UsersAndGroups.qml" line="605"/>
-        <source>Home directory</source>
-        <translation>Diretório Home</translation>
-    </message>
-    <message>
-        <location filename="../UsersAndGroups.qml" line="299"/>
-        <source>Enable account expiration</source>
-        <translation>Ativar expiração de conta</translation>
-    </message>
-    <message>
-        <location filename="../UsersAndGroups.qml" line="307"/>
-        <source>Account expires</source>
-        <translation>Conta expira</translation>
-    </message>
-    <message>
-        <location filename="../UsersAndGroups.qml" line="329"/>
-        <source>Lock user account</source>
-        <translation>Bloquear de conta de usuário</translation>
-    </message>
-    <message>
-        <location filename="../UsersAndGroups.qml" line="336"/>
-        <source>Days before change allowed</source>
-        <translation>Dias antes da mudança ser permitida</translation>
-    </message>
-    <message>
-        <location filename="../UsersAndGroups.qml" line="350"/>
-        <source>Days before change required</source>
-        <translation>Dias antes da mudança ser requerida</translation>
-    </message>
-    <message>
-        <location filename="../UsersAndGroups.qml" line="364"/>
-        <source>Days warning before change</source>
-        <translation>Dias de aviso antes da mudança</translation>
-    </message>
-    <message>
-        <location filename="../UsersAndGroups.qml" line="378"/>
-        <source>Days before account inactive</source>
-        <translation>Dias antes da conta ficar inativa</translation>
-    </message>
-    <message>
-        <location filename="../UsersAndGroups.qml" line="393"/>
-        <source>Select the groups that the user will be a member of</source>
-        <translation>Selecione os grupos que o usuário será membro</translation>
-    </message>
-    <message>
-        <location filename="../UsersAndGroups.qml" line="461"/>
-        <location filename="../UsersAndGroups.qml" line="907"/>
-        <source>Delete</source>
-        <translation>Excluir</translation>
-    </message>
-    <message>
-        <location filename="../UsersAndGroups.qml" line="477"/>
-        <location filename="../UsersAndGroups.qml" line="676"/>
-        <location filename="../UsersAndGroups.qml" line="923"/>
-        <location filename="../UsersAndGroups.qml" line="1022"/>
-        <source>Save</source>
-        <translation>Salvar</translation>
-    </message>
-    <message>
-        <location filename="../UsersAndGroups.qml" line="487"/>
-        <location filename="../UsersAndGroups.qml" line="687"/>
-        <location filename="../UsersAndGroups.qml" line="933"/>
-        <location filename="../UsersAndGroups.qml" line="1032"/>
-        <source>Close</source>
-        <translation>Fechar</translation>
-    </message>
-    <message>
-        <location filename="../UsersAndGroups.qml" line="598"/>
         <source>Create home directory</source>
         <translation>Crie o diretório home</translation>
     </message>
     <message>
-        <location filename="../UsersAndGroups.qml" line="625"/>
+        <location filename="../UsersAndGroups.qml" line="286"/>
+        <location filename="../UsersAndGroups.qml" line="492"/>
+        <source>Home directory</source>
+        <translation>Diretório Home</translation>
+    </message>
+    <message>
+        <location filename="../UsersAndGroups.qml" line="306"/>
         <source>Create a private group for the user</source>
         <translation>Criar um grupo privado para o usuário</translation>
     </message>
     <message>
-        <location filename="../UsersAndGroups.qml" line="638"/>
+        <location filename="../UsersAndGroups.qml" line="319"/>
         <source>Specify user ID manually</source>
         <translation>Especificar o ID de usuário manualmente</translation>
     </message>
     <message>
-        <location filename="../UsersAndGroups.qml" line="646"/>
+        <location filename="../UsersAndGroups.qml" line="327"/>
         <source>User ID</source>
         <translation>ID de usuário</translation>
     </message>
     <message>
-        <location filename="../UsersAndGroups.qml" line="825"/>
-        <location filename="../UsersAndGroups.qml" line="964"/>
+        <location filename="../UsersAndGroups.qml" line="346"/>
+        <location filename="../UsersAndGroups.qml" line="693"/>
+        <location filename="../UsersAndGroups.qml" line="920"/>
+        <location filename="../UsersAndGroups.qml" line="1061"/>
+        <source>Save</source>
+        <translation>Salvar</translation>
+    </message>
+    <message>
+        <location filename="../UsersAndGroups.qml" line="356"/>
+        <location filename="../UsersAndGroups.qml" line="702"/>
+        <location filename="../UsersAndGroups.qml" line="929"/>
+        <location filename="../UsersAndGroups.qml" line="1070"/>
+        <source>Close</source>
+        <translation>Fechar</translation>
+    </message>
+    <message>
+        <location filename="../UsersAndGroups.qml" line="407"/>
+        <source>User last changed password on</source>
+        <translation>Usuário modificou sua senha pela última vez em</translation>
+    </message>
+    <message>
+        <location filename="../UsersAndGroups.qml" line="512"/>
+        <source>Enable account expiration</source>
+        <translation>Ativar expiração de conta</translation>
+    </message>
+    <message>
+        <location filename="../UsersAndGroups.qml" line="520"/>
+        <source>Account expires</source>
+        <translation>Conta expira</translation>
+    </message>
+    <message>
+        <location filename="../UsersAndGroups.qml" line="542"/>
+        <source>Lock user account</source>
+        <translation>Bloquear de conta de usuário</translation>
+    </message>
+    <message>
+        <location filename="../UsersAndGroups.qml" line="549"/>
+        <source>Days before change allowed</source>
+        <translation>Dias antes da mudança ser permitida</translation>
+    </message>
+    <message>
+        <location filename="../UsersAndGroups.qml" line="563"/>
+        <source>Days before change required</source>
+        <translation>Dias antes da mudança ser requerida</translation>
+    </message>
+    <message>
+        <location filename="../UsersAndGroups.qml" line="577"/>
+        <source>Days warning before change</source>
+        <translation>Dias de aviso antes da mudança</translation>
+    </message>
+    <message>
+        <location filename="../UsersAndGroups.qml" line="591"/>
+        <source>Days before account inactive</source>
+        <translation>Dias antes da conta ficar inativa</translation>
+    </message>
+    <message>
+        <location filename="../UsersAndGroups.qml" line="605"/>
+        <source>Select the groups that the user will be a member of</source>
+        <translation>Selecione os grupos que o usuário será membro</translation>
+    </message>
+    <message>
+        <location filename="../UsersAndGroups.qml" line="684"/>
+        <location filename="../UsersAndGroups.qml" line="1052"/>
+        <source>Delete</source>
+        <translation>Excluir</translation>
+    </message>
+    <message>
+        <location filename="../UsersAndGroups.qml" line="818"/>
+        <source>GID</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../UsersAndGroups.qml" line="827"/>
+        <location filename="../UsersAndGroups.qml" line="873"/>
+        <location filename="../UsersAndGroups.qml" line="963"/>
         <source>Group name</source>
         <translation>Nome do grupo</translation>
     </message>
     <message>
-        <location filename="../UsersAndGroups.qml" line="839"/>
-        <source>Select the user to join this group</source>
-        <translation>Selecione o usuário para entar neste grupo</translation>
+        <location filename="../UsersAndGroups.qml" line="835"/>
+        <source>Members</source>
+        <translation>Membros</translation>
     </message>
     <message>
-        <location filename="../UsersAndGroups.qml" line="984"/>
+        <location filename="../UsersAndGroups.qml" line="891"/>
         <source>Specify group ID manually</source>
         <translation>Especificar o ID do grupo manualmente</translation>
     </message>
     <message>
-        <location filename="../UsersAndGroups.qml" line="992"/>
+        <location filename="../UsersAndGroups.qml" line="899"/>
         <source>Group Id</source>
         <translation>ID do grupo</translation>
+    </message>
+    <message>
+        <location filename="../UsersAndGroups.qml" line="977"/>
+        <source>Select the user to join this group</source>
+        <translation>Selecione o usuário para entar neste grupo</translation>
     </message>
 </context>
 </TS>
